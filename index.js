29d5a6c34db6cc0232fee65d7364d4761046d6f3
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-27 19:53:44 +0500

    update it

---
+++ index.js
@@ -0,0 +1,19 @@
+function halfFryEgg() {
+    var cooked = 1 + 1.5 + 2;
+    // salt + butter + egg
+    console.log(cooked);
+    return cooked;
+}
+// halfFryEgg();
+// const respnse = halfFryEgg(2, 4, 10);
+// console.log(respnse); // invoking function
+// function myTeachers(
+//   teacher1: string,
+//   teacher2: string,
+//   teacher3: string,
+//   teacher4: string
+// ) {
+//   return teacher1 + teacher2 + teacher3 + teacher4;
+// }
+// const response = myTeachers("Asharib", "Ubaid", "Hafsa", "Naeem");
+// console.log(response);
---
+++ index.ts
@@ -0,0 +1,22 @@
+function halfFryEgg(): number {
+  //after parenthensis is the type of return if type is define the must use the return
+  let cooked: number = 1 + 1.5 + 2;
+  // salt + butter + egg
+  console.log(cooked);
+  return cooked;
+}
+halfFryEgg();
+
+const respnse = halfFryEgg();
+console.log(respnse); // invoking function
+function myTeachers(
+  teacher1: string,
+  teacher2: string,
+  teacher3: string,
+  teacher4: string
+) {
+  return teacher1 + teacher2 + teacher3 + teacher4;
+}
+
+const response = myTeachers("Asharib", "Ubaid", "Hafsa", "Naeem");
+console.log(response);
